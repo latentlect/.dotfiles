dd31aa02cb50fe339a47f31343cc133dc199d84d
latentlect <101093796+latentlect@users.noreply.github.com>
2022-11-17 02:06:23 +0000

    set username

--- .utils/create_videoclip_dir.sh
+++ .utils/create_videoclip_dir.sh
@@ -7,8 +7,12 @@ create_mpv_videoclip_dir(){
 		# replace user with username
 		sed -i "s/\<user\>/$(whoami)/" "$HOME/.config/mpv/script-opts/videoclip.conf"
     	sed -i "s/\<user\>/$(whoami)/" "$HOME/.config/mpv/mpv.conf"
+
+		sed -i "s/\<uname\>/$(whoami)/" "$HOME/.config/mpv/script-opts/youtube-download.conf"
 	fi
 }
 
 
 create_mpv_videoclip_dir
+
+
